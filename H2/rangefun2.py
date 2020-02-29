cd584b14dddd066b988fcdb44a13d0ef08b5caa8
Karishma Pokhrel <karishma.pokhrel@bestbuy.com>
2020-02-29 12:29:38 -0600

    H2 - rangefun.py working

---
+++ H2/rangefun2.py
@@ -0,0 +1,38 @@
+for num in range(1, 11, 1):
+    print(num, end=' ')
+print ()
+
+for num in range(1, 11, 2):
+    print(num, end=' ')
+print ()
+
+for num in range(47, 0, -46):
+    print(num, end=' ')
+print ()
+
+for num in range(10, 0, -1):
+    print(num, end=' ')
+print ()
+
+for num in range(10, 0, -3):
+    print(num, end=' ')
+print ()
+
+for num in range(1, 22, 4):
+    print(num, end=' ')
+print ()
+
+'''
+/Users/a6003261/Documents/class/pythonProjects/venv/bin/python /Users/a6003261/Documents/class/pythonProjects/H2/rangefun2.py
+1 2 3 4 5 6 7 8 9 10 
+1 3 5 7 9 
+47 1 
+10 9 8 7 6 5 4 3 2 1 
+10 7 4 1 
+1 5 9 13 17 21 
+
+Process finished with exit code 0
+'''
+
+
+
