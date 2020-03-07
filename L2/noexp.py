3cf9532fa18e8f87cd0857a3fe93bf5637ad1825
Karishma Pokhrel <karishma.pokhrel@bestbuy.com>
2020-03-07 11:15:37 -0600

    L2 noexp.py working

---
+++ L2/noexp.py
@@ -0,0 +1,17 @@
+x = input('Enter x')
+n = input('Enter n')
+
+result = 1
+for counter in range(int(n)):
+    result = result * int(x)
+
+print(result)
+
+'''
+/Users/a6003261/Documents/class/pythonProjects/venv/bin/python /Users/a6003261/Documents/class/pythonProjects/L2/noexp.py
+Enter x3
+Enter n4
+81
+
+Process finished with exit code 0
+'''
