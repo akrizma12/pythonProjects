9284cbea38f517f79e8bb1eb7f464e2781661a3f
Karishma Pokhrel <karishma.pokhrel@bestbuy.com>
2020-03-07 11:39:05 -0600

    L2/rangefun0.py working

---
+++ L2/rangefun0.py
@@ -0,0 +1,29 @@
+a = input("Enter a")
+b = input("Enter b")
+c = input("Enter c")
+
+for num in range(int(a), int(b), int(c)):
+    print(num)
+
+'''
+/Users/a6003261/Documents/class/pythonProjects/venv/bin/python /Users/a6003261/Documents/class/pythonProjects/L2/rangefun0.py
+Enter a2
+Enter b7
+Enter c1
+2
+3
+4
+5
+6
+
+Process finished with exit code 0
+
+/Users/a6003261/Documents/class/pythonProjects/venv/bin/python /Users/a6003261/Documents/class/pythonProjects/L2/rangefun0.py
+Enter a2
+Enter b5
+Enter c-1
+
+Process finished with exit code 0
+'''
+
+
