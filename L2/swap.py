775216625d281efe4642d04dbac886743d13526f
Karishma Pokhrel <karishma.pokhrel@bestbuy.com>
2020-03-07 09:33:38 -0600

    H2/swap/py working

--- L2/swap.py
+++ L2/swap.py
@@ -14,3 +14,13 @@ input2 = temp
 
 print('After swap values are (vars input1, input2 after swap)', input1, input2)
 
+'''
+/Users/a6003261/Documents/class/pythonProjects/venv/bin/python /Users/a6003261/Documents/class/pythonProjects/L2/swap.py
+Enter first integer1
+Enter second integer2
+Entered values (vars input1, input2)= 1 2
+After swap values are (vars input1, input2 after swap) 2 1
+
+Process finished with exit code 0
+'''
+
